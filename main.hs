18536e400852a3ff0054f774b6a7090f24fa33af
miguel.chauca@almundo.com <miguel.chauca@almundo.com>
2018-04-21 10:15:56 -0300

    primer tag v1

---
+++ main.hs
@@ -0,0 +1,9 @@
+doble = (*2);
+cuandruple = doble.doble
+factorial numero 
+        | numero == 0 = 1
+        | otherwise = factorial(numero-1)*numero
+
+factorial2 numero = factorial2(numero-1)*numero
+nombrePar :: String -> Bool
+nombrePar = even.length
