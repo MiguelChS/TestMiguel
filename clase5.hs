77d62e374352efc40b1e7efb6b7a18f40ab59f1c
miguel.chauca@almundo.com <miguel.chauca@almundo.com>
2018-05-12 09:50:00 -0300

    clase 5

---
+++ clase5.hs
@@ -0,0 +1,11 @@
+--fold
+numero = [1..20000];
+
+--condicones
+-- 1 mayores que 5000
+-- 2 menores o iguales que 10000
+-- 3 empiece con 503
+
+-- fold_ funcion -- semilla ? que carajo es la semilla es mi punto de partida
+-- foldr filter semilla lista
+-- foldl
